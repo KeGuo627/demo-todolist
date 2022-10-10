519de941845c929fa2ee4b38b50b12efe9cddcb6
Ke Guo (Olivia) <66099237+KeGuo627@users.noreply.github.com>
2022-10-10 14:00:12 -0700

    Adding totoggle when change todo.done to true

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     this.state = {
       todos: [],
       value: "",
+      newId: 0,
     };
   }
 
@@ -13,12 +14,15 @@ class App extends Component {
     e.preventDefault();
     const obj = {
       name: this.state.value,
-
-      id: Date.now(),
+      isCompleted: false,
+      id: this.state.newId,
     };
     console.log(obj);
     if (this.state.value !== "") {
-      this.setState({ todos: this.state.todos.concat(obj) });
+      this.setState({
+        todos: this.state.todos.concat(obj),
+        newId: this.state.newId + 1,
+      });
       this.setState({ value: "" });
     }
   };
@@ -28,10 +32,25 @@ class App extends Component {
       todos: [...this.state.todos].filter((item) => item.id !== itemId),
     });
   };
+  
+  onToggle = (itemId) => {
+    const updatedTodos = JSON.parse(JSON.stringify(this.state.todos));
+    //console.log(updatedTodos);
+    //console.log(itemId);
+    updatedTodos[itemId].isCompleted = !updatedTodos[itemId].isCompleted;
+    //console.log(updatedTodos[itemId]);
+
+    this.setState({
+      todos: updatedTodos,
+    });
+  };
 
   render() {
     const mylist = this.state.todos.map((todo) => (
-      <li key={todo.id} className="todo_item">
+      <li key={todo.id} className={todo.isCompleted ? "task-done" : "task-incompleted"}
+        onDoubleClick={() => {
+          this.onToggle(todo.id);
+        }}>
         {todo.name}
 
         <button onClick={() => this.onDeleteTask(todo.id)}>Delete</button>
